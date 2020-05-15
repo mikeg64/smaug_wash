bb6d94fa5dfc7995d759610dba84e4e772bec9aa
mikeg64 <m.griffiths@sheffield.ac.uk>
2020-05-15 09:39:16 +0100

    mov3 changing initial position of source

---
+++ runwashingmachine_mov3h12p5Mm_kg_uni.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#$ -j y
+#$ -V 
+#$ -l arch=intel*
+#$ -l gpu=1,gpu_arch=nvidia-k40m
+##$ -l gpu=1,gpu_arch=nvidia-m2070
+
+##$ -P cs-test
+##$ -P mhd
+#$ -P gpu
+#$ -N washm_mov3
+#$ -l mem=12G
+#$ -l rmem=12G
+#$ -l h_rt=168:00:00
+module add libs/cuda/6.5.14
+
+#cp solarmodels/Makefile.spicule src/Makefile
+#cp solarmodels/make_inputs.spicule src/make_inputs
+#cp solarmodels/iosac2.5d.cpp.spicule src/iosac2.5d.cpp
+
+
+#cd src
+#make clean
+#make smaug
+#cd ..
+
+cd include
+cp iosmaugparams.washing_mach_mov3_h12p5Mm_kg_uni.h iosmaugparams.h
+cp initialisation_user.h.wash_180 initialisation_user.h
+cd ..
+
+#rm bin/iosmaug.o
+cd src
+cp usersource.washing_mach_mov3_h12p5Mm_kg_uni.cu usersource.cu
+
+cp boundary_3d.cu boundary.cu
+make clean
+#rm centdiff1.o
+#rm centdiff2.o
+#make -f Makefile_3d smaug
+make -f Makefile_3d_k40 smaug
+cd ..
+
+
+
+export TIMECOUNTER=0
+source timeused
+#bin/smaug a init
+bin/smaug a
+source timeused
+
