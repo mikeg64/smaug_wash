8247a1dcf99140dbbef1f32e7d7ed08564672761
mikeg64 <m.griffiths@sheffield.ac.uk>
2020-11-11 20:05:40 +0000

    model un1 rerun uni5 aa/2    uni6 2*aa

---
+++ runwashingmachine_180h12p5Mm_kg_uni6.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#$ -j y
+#$ -V 
+#$ -l arch=intel*
+#$ -l gpu=1,gpu_arch=nvidia-k40m
+##$ -l gpu=1,gpu_arch=nvidia-m2070
+
+##$ -P cs-test
+##$ -P mhd
+##$ -P gpu
+#$ -N washingmachine
+#$ -l mem=12G
+#$ -l rmem=12G
+#$ -l h_rt=168:00:00
+module add libs/cuda/6.5.14
+
+#cp solarmodels/Makefile.spicule src/Makefile
+#cp solarmodels/make_inputs.spicule src/make_inputs
+#cp solarmodels/iosac2.5d.cpp.spicule src/iosac2.5d.cpp
+
+
+#cd src
+#make clean
+#make smaug
+#cd ..
+
+cd include
+cp iosmaugparams.washing_mach_180_h12p5Mm_kg_uni6.h iosmaugparams.h
+cp initialisation_user.h.wash_180 initialisation_user.h
+cd ..
+
+#rm bin/iosmaug.o
+cd src
+cp usersource.washing_mach_180_h12p5Mm_kg_uni6.cu usersource.cu
+
+cp boundary_3d.cu boundary.cu
+make clean
+#rm centdiff1.o
+#rm centdiff2.o
+#make -f Makefile_3d smaug
+make -f Makefile_3d_k40 smaug
+cd ..
+
+
+
+export TIMECOUNTER=0
+source timeused
+#bin/smaug a init
+bin/smaug a
+source timeused
+
