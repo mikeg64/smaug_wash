bb88023ee9992b1cb17ee1355f21b0ffcb8346ba
mikeg64 <m.griffiths@sheffield.ac.uk>
2021-07-30 08:33:03 +0100

    nvivo tracking of papers and data

---
+++ matlab/temp1.m
@@ -0,0 +1,16 @@
+   
+
+
+evelchrom1_bz = evelchrom1_bz  +      evelchrom_bz;
+evelcor1_bz =   evelcor1_bz  +    evelcor_bz;   
+eveltran1_bz=   eveltran1_bz  +           eveltran_bz     ;
+evelv1Mm1_bz =  evelv1Mm1_bz       +              evelv1Mm_bz       ;
+evelv2Mm1_bz=   evelv2Mm1_bz    +         evelv2Mm_bz   ;
+evelvp5Mm1_bz =   evelvp5Mm1_bz   +          evelvp5Mm_bz     ;
+evelchrom1_vh =   evelchrom1_vh     +                 evelchrom_vh    ;
+evelcor1_vh =     evelcor1_vh         +          evelcor_vh;
+eveltran1_vh=     eveltran1_vh         +            eveltran_vh           ;
+evelv1Mm1_v=      evelv1Mm1_v       +             evelv1Mm_v ;
+evelv2Mm1_v =     evelv2Mm1_v          +              evelv2Mm_v       ;
+evelvp5Mm1_v  =    evelvp5Mm1_v             +                   evelvp5Mm_v    ;
+ 
