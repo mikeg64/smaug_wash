b68c4189c6a03cd5be9324f3c22d6b3c4235fd3e
mikeg64 <m.griffiths@sheffield.ac.uk>
2021-12-12 21:08:48 +0000

    plot section surfvzsec and compute amplitude ratios

--- matlab/ampratios.m
+++ matlab/ampratios.m
@@ -1,5 +1,10 @@
-
-%routine to locate peaks
+% compute the ampitude ratios
+%see note .  https://community.researchspace.com/notebookEditor/1428875?initialRecordToDisplay=1428876&settingsKey=gEbpzzwlHn
+%
+% as described in
+%
+% https://ui.adsabs.harvard.edu/abs/2013A%26A...551A.137M/abstract
+%
 
 directory='/shared/sp2rc2/Shared/simulations/smaug_realpmode/fastdata/cs1mkg/smaug/spic_5b2_2_bv100G/';
 extension='.out';
@@ -13,7 +18,11 @@ rdirectory='/washmc-data/uni11';
 directory=[bdir,rdirectory,'/'];
 
 
+B0=0.15;
 
+gamma=1.666667;
+
+mu=4.0*pi*1.0e-7;
 
 %matfile=[directory,'vvzverustime.mat'];
 matfile=[directory,'hhzverustime1.mat'];
@@ -21,48 +30,6 @@ load(matfile);
 nt=533;
 evelv=evelv2Mm_v;
 evelb=evelv2Mm_bz;
-figure(1) 
-%subplot(1,3,1)
-ss1=reshape(evelv(62,:,1:nt),[124,nt]);
-ss1f=ss1(62,:);
-plot(log(abs(ss1(62,:))))
-hold on
-
-%subplot(1,3,2)
-
-ss2=reshape(10^5*evelb(:,62,1:nt),[124,nt]);
-ss2f=ss2(62,:);
-plot(log(abs(ss2(62,:))))
-
-hold on
-%subplot(1,3,3)
-
-%find peaks separated at least 30s
-[pksvz,locsvz]=findpeaks(ss1(62,:),'MinPeakDistance',30);
-[pksbz,locsbz]=findpeaks(ss2(62,:),'MinPeakDistance',30);
-
-plot(locsvz,pksvz,'d')
-plot(locsbz,pksbz,'o')
-
-xlim([0 600])
-ylim([-20 20])
-%least squares fit
-t=(48:1:nt)';
-tbl = table(log(abs(ss1f(48:nt)')),t);
-mdl = fitlm(tbl,'linear');
-
-coeff=mdl.Coefficients.Estimate;
-b=coeff(2); %slope
-a=coeff(1); %intercept
-
-shift1f=ss1f(48:1:nt)'-exp(-b.*t+a);
-
-xtest=(48:1:nt);
-ytest=b.*xtest+a;
-figure(2)
-plot(shift1f);
-hold on
-plot(xtest,ytest,'x');
 
 t1=500;
 t2=570;
@@ -70,13 +37,42 @@ t2=570;
 evelv=evelv1Mm_v;
 evelb=evelv1Mm_bz;
 
+% cs=sqrt(gamma p/rho)
+%
+% 
+
+%rho 1
+% m1 2
+% m2 3
+% m3 4
+% e 5
+% bx 6
+% by 7
+% bz 8
+% eb 9
+% rhob 10
+% b1b 11
+% b2b 12
+% b3b 13
+
+
+ val1=reshape(wd(2,nrange,nrange,nrange),124,124,124);%z component vel
+ val2=reshape(wd(1,nrange,nrange,nrange)+wd(10,nrange,nrange,nrange),124,124,124); %density
+ val3=reshape(wd(1,nrange,nrange,nrange)+wd(10,nrange,nrange,nrange),124,124,124); %bz
+ val4=reshape(wd(1,nrange,nrange,nrange)+wd(10,nrange,nrange,nrange),124,124,124); %energy
+
+
+    val1=(val1./val2);
+
+
+
 
 mod=reshape(evelv(:,:,t1),[124,124]);
 mod1=reshape(evelv(:,:,t2),[124,124]);
 mod2=reshape(evelb(:,:,t1),[124,124]);
 mod3=reshape(evelb(:,:,t2),[124,124]);
 
-figure(3);
+figure(1);
 subplot(2,2,1); surf(real(mod));  shading interp;   title('v '+string(t1));
 view(0,90);
 colorbar();
@@ -101,7 +97,7 @@ mod1=reshape(evelv(:,:,t2),[124,124]);
 mod2=reshape(evelb(:,:,t1),[124,124]);
 mod3=reshape(evelb(:,:,t2),[124,124]);
 
-figure(4);
+figure(2);
 subplot(2,2,1); surf(real(mod));  shading interp;   title('v '+string(t1));
 view(0,90);
 colorbar();
@@ -115,7 +111,7 @@ subplot(2,2,4); surf(real(mod3));  shading interp;   title('b '+string(t2));
 view(0,90);
 colorbar();
 
-figure(5);
+figure(3);
 
 evelv=evelv1Mm_v;
 evelb=evelv1Mm_bz;
